af6cf8c982f58287213b089ed40bd7c43469a84e
TH <kareme007@gmail.com>
2025-01-21 18:00:08 +0900

    2025-01-21

--- src/components/main.js
+++ src/components/main.js
@@ -1,36 +1,49 @@
 import { Map, MapMarker } from "react-kakao-maps-sdk";
-import { MainMapContainer, MainText, MainWrap } from "../style/mainStyle";
+import {
+  MainContentsContainer,
+  MainMapContainer,
+  MainText,
+  MainTextContainer,
+  MainTitle,
+  MainWrap,
+} from "../style/mainStyle";
 
-const center = {lat : 35.87514124558859, lng : 128.81137868953445}
+const center = { lat: 35.87514124558859, lng: 128.81137868953445 };
 
 const MyMap = () => {
-    return (
-        <Map // 지도를 표시할 Container
-        center={center}
-        style={{
-          // 지도의 크기
-          width: "500px",
-          height: "500px",
-        }}
-        level={4} // 지도의 확대 레벨
-      >
-        <MapMarker // 마커를 생성합니다
-          position={center}
-        />
-      </Map>
-    );
-}
+  return (
+    <Map // 지도를 표시할 Container
+      center={center}
+      style={{
+        // 지도의 크기
+        width: "500px",
+        height: "500px",
+      }}
+      level={4} // 지도의 확대 레벨
+    >
+      <MapMarker // 마커를 생성합니다
+        position={center}
+      />
+    </Map>
+  );
+};
 
 const Main = () => {
-
-    return (
-        <MainWrap>
-            <MainMapContainer>
-                <MyMap></MyMap>
-            </MainMapContainer>
-            <MainText>소개글</MainText>
-        </MainWrap>
-    );
-}
+  return (
+    <MainWrap>
+      <MainTitle>오시는 길</MainTitle>
+      <MainContentsContainer>
+        <MainMapContainer>
+          <MyMap></MyMap>
+        </MainMapContainer>
+        <MainTextContainer>
+            <MainText>하루헤어</MainText>
+            <MainText>예시 텍스트</MainText>
+            <MainText>예시 텍스트22</MainText>
+        </MainTextContainer>
+      </MainContentsContainer>
+    </MainWrap>
+  );
+};
 
 export default Main;
--- src/style/mainStyle.js
+++ src/style/mainStyle.js
@@ -7,18 +7,33 @@ export const BodyWrap = styled.div`
   height: 1200px;
   background-color: #9bd2ff;
   grid-template-rows: 100px 233px auto 100px;
-  grid-template-areas: 
-  "header"
-  "banner"
-  "main"
-  "footer";
-  `;
+  grid-template-areas:
+    "header"
+    "banner"
+    "main"
+    "footer";
+`;
 
 export const MainWrap = styled.div`
   width: 100%;
   height: auto;
   background-color: beige;
   grid-area: main;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const MainTitle = styled.div`
+  width: 100%;
+  height: 100px;
+  font-size: 20px;
+  text-align: center;
+`;
+
+export const MainContentsContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -32,12 +47,14 @@ export const MainMapContainer = styled.div`
   background-color: blue;
 `;
 
-export const MainText = styled.div`
+export const MainTextContainer = styled.div`
   width: 500px;
   height: 500px;
   background-color: aliceblue;
 `;
 
-export const MainTitle = styled.div`
-  
+export const MainText = styled.div`
+  padding: 10px 5px;
+  width: 100%;
+  min-height: 30px;
 `;
